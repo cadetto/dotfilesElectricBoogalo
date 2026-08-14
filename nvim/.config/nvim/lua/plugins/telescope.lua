fbe7d7df48d26d834e9b0e7b9fdbf0f131ff13b2
Christian Cadetto <ccadetto@logoscorp.com>
2026-08-14 14:59:34 -0400

    Update configs

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -6,7 +6,8 @@ return {
     keys = {
         { '<leader>pf', function() require('telescope.builtin').find_files() end },
         { '<C-p>',      function() require('telescope.builtin').git_files() end },
-        { '<leader>ps', function() require('telescope.builtin').grep_string({ search = vim.fn.input('Grep >') }) end }
+        { '<leader>ps', function() require('telescope.builtin').grep_string({ search = vim.fn.input('Grep >') }) end },
+        { '<leader>pg', function() require('telescope.builtin').live_grep() end, desc = "Live Grep" }
     },
 
     opts = {},
